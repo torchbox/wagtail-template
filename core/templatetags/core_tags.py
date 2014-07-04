8e27e0dab579b5b2789781420d3d199bfcc45ca2
Karl Hobley <karl@torchbox.com>
2014-07-04 09:09:15 +0100

    Fixes to core_tags

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -4,26 +4,12 @@ from django.template.defaultfilters import slugify
 
 from wagtail.wagtailcore.utils import camelcase_to_underscore
 
-from core.models import *
 
 register = template.Library()
 
-# Return the kind of field as a string e.g boolean_field
-# Usage: {{ field|field_type }}
-@register.filter
-def field_type(bound_field):
-    return camelcase_to_underscore(bound_field.field.__class__.__name__)
-
-
-# Return the kind of field widget as a string e.g checkbox_input
-# Usage: {{ field|widget_type }}
-@register.filter
-def widget_type(bound_field):
-    return camelcase_to_underscore(bound_field.field.widget.__class__.__name__)
-
 
 # Return the model name/"content type" as a css-friendly string e.g blog-page, news-listing-page
-# Usage: {{ self|content_type_slugified }}
+# Usage: {% verbatim %}{{ self|content_type_slugified }}{% endverbatim %}
 @register.filter
 def content_type_slugified(model):
     return slugify(model.__class__.__name__)
