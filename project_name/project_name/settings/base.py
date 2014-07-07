dfc5b7da7d8cc27b0734fdb37994738065558ce0
Karl Hobley <karl@torchbox.com>
2014-07-07 14:39:41 +0100

    Moved some production settings into base settings

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -155,9 +155,42 @@ TEMPLATE_DIRS = (
 )
 
 
+# Celery settings
+# http://celery.readthedocs.org/en/latest/configuration.html
+
+import djcelery
+
+djcelery.setup_loader()
+
+CELERY_SEND_TASK_ERROR_EMAILS = True
+BROKER_URL = 'redis://'
+
+
+# Use Redis as the cache backend for extra performance
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379:1',
+        'KEY_PREFIX': '{{ project_name }}',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    }
+}
+
+
 # Wagtail settings
 
 LOGIN_URL = 'wagtailadmin_login'
 LOGIN_REDIRECT_URL = 'wagtailadmin_home'
 
 WAGTAIL_SITE_NAME = "{{ project_name }}"
+
+# Use Elasticsearch as the search backend for extra performance and better search results
+WAGTAILSEARCH_BACKENDS = {
+    'default': {
+        'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch.ElasticSearch',
+        'INDEX': '{{ project_name }}',
+    },
+}
--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -13,41 +13,6 @@ TEMPLATE_DEBUG = False
 COMPRESS_OFFLINE = True
 
 
-# Celery settings
-# http://celery.readthedocs.org/en/latest/configuration.html
-
-import djcelery
-
-djcelery.setup_loader()
-
-CELERY_SEND_TASK_ERROR_EMAILS = True
-BROKER_URL = 'redis://'
-
-
-# Use Redis as the cache backend for extra performance
-
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379:1',
-        'KEY_PREFIX': '{{ project_name }}',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-        }
-    }
-}
-
-
-# Use Elasticsearch as the search backend for extra performance and better search results
-
-WAGTAILSEARCH_BACKENDS = {
-    'default': {
-        'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch.ElasticSearch',
-        'INDEX': '{{ project_name }}',
-    },
-}
-
-
 try:
     from .local import *
 except ImportError:
