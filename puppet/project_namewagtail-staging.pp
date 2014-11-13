a2f4d221044168ee94c783e1e662f826a89bbc25
Karl Hobley <karl@torchbox.com>
2014-11-13 10:47:41 +0000

    Added puppet configuration

---
+++ puppet/project_namewagtail-staging.pp
@@ -0,0 +1,38 @@
+# vim:ts=4 sw=4 et:
+class wagtail::site::staging::{{ project_name }}wagtail inherits wagtail::site::staging {
+    wagtail::app { '{{ project_name }}wagtail':
+        ip               => $ipaddress,
+        ip6              => $ipaddress6,
+        manage_ip        => false,
+        manage_db        => true,
+        manage_user      => true,
+        appsubdir        => '{{ project_name }}',
+        settings         => '{{ project_name }}/settings',
+        wsgi_module      => '{{ project_name }}.wsgi',
+        requirements     => 'requirements.txt',
+        servername       => '{{ project_name }}-staging.torchboxapps.com',
+        alias_redirect   => false,
+        codebase_project => '', # CHANGEME
+        codebase_repo    => '', # CHANGEME
+        git_uri          => 'CODEBASE',
+        django_version   => '1.7',
+        staticdir        => "static",
+        mediadir         => "media",
+        deploy           => [ '@admin' ], # CHANGEME
+        admins           => {
+            # CHANGEME
+            # List of users to send error emails to. Eg:
+            # 'Joe Bloggs' => 'joe.bloggs@torchbox.com',
+        },
+        nagios_url       => '/',
+        auth => {
+            enabled       => true,
+            hosts         => [ 'tbx' ],
+            users         => {
+                # CHANGEME
+                # This is the credentials for HTTP authentication. Eg:
+                # 'username'  => 'password',
+            },
+        },
+    }
+}
