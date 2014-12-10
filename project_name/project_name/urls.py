b0df37990565d0ac5f11e0b0b5e5ef1af175345d
Karl Hobley <karlhobley10@gmail.com>
2014-12-10 09:51:39 +0000

    Serve all media from development server
    Previously only served the /media/images folder

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,5 +1,3 @@
-import os
-
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
@@ -29,4 +27,4 @@ if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
