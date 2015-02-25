a2e81d132ab123600cbab96d71fccddf07a47631
Karl Hobley <karlhobley10@gmail.com>
2015-02-25 16:54:00 +0000

    New Vagrant box (#29)

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,7 +2,7 @@
 
 PROJECT_NAME=$1
 
-PROJECT_DIR=/home/vagrant/$PROJECT_NAME
+PROJECT_DIR=/vagrant
 DJANGO_DIR=$PROJECT_DIR/$PROJECT_NAME
 VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
 
@@ -11,15 +11,13 @@ PIP=$VIRTUALENV_DIR/bin/pip
 
 
 # Create database
-createdb -Upostgres $PROJECT_NAME
+su - vagrant -c "createdb $PROJECT_NAME"
 
 
 # Virtualenv setup for project
-su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR && \
-    echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
-    PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache $PIP install -r $PROJECT_DIR/requirements.txt"
-
-echo "workon $PROJECT_NAME" >> /home/vagrant/.bashrc
+su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR"
+su - vagrant -c "echo $PROJECT_DIR > $VIRTUALENV_DIR/.project"
+su - vagrant -c "$PIP install -r $PROJECT_DIR/requirements.txt"
 
 
 # Set execute permissions on manage.py as they get lost if we build from a zip file
@@ -33,6 +31,13 @@ su - vagrant -c "$PYTHON $DJANGO_DIR/manage.py migrate --noinput && \
 
 # Add a couple of aliases to manage.py into .bashrc
 cat << EOF >> /home/vagrant/.bashrc
-alias dj="$PYTHON $DJANGO_DIR/manage.py"
+export PYTHONPATH=$DJANGO_DIR
+export DJANGO_SETTINGS_MODULE=$PROJECT_NAME.settings.dev
+
+alias dj="django-admin"
 alias djrun="dj runserver 0.0.0.0:8000"
+
+source $VIRTUALENV_DIR/bin/activate
+export PS1="[$PROJECT_NAME \W]\\$ "
+cd $PROJECT_DIR
 EOF
