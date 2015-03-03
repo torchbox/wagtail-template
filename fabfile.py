2cbf3e2ced54d09940ca0e4485c73678476f40f6
Karl Hobley <karlhobley10@gmail.com>
2015-03-03 14:18:53 +0000

    Simplified fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,21 +9,15 @@ env.roledefs = {
 
 @roles('staging')
 def deploy_staging():
-    # Remove this line when you're happy that this Fabfile is correct
+    # Remove this line when you're happy that this task is correct
     raise RuntimeError("Please check the fabfile before using it")
 
-    base_dir = '/usr/local/django/{{ project_name }}wagtail/'
-    virtualenv_dir = '/usr/local/django/virtualenvs/{{ project_name }}wagtail/'
-    python = virtualenv_dir + '/bin/python'
-    pip = virtualenv_dir + '/bin/pip'
-
-    with cd(base_dir):
-        run('git pull origin master')
-        run(pip + ' install -r requirements.txt')
-        run(python + ' manage.py migrate --settings={{ project_name }}.settings.production --noinput')
-        run(python + ' manage.py collectstatic --settings={{ project_name }}.settings.production --noinput')
-        run(python + ' manage.py compress --settings={{ project_name }}.settings.production')
-        run(python + ' manage.py update_index --settings={{ project_name }}.settings.production')
+    run('git pull origin staging')
+    run('pip install -r requirements.txt')
+    run('django-admin migrate --noinput')
+    run('django-admin collectstatic --noinput')
+    run('django-admin compress')
+    run('django-admin update_index')
 
     # 'restart' should be an alias to a script that restarts the web server
     run('restart')
@@ -31,21 +25,15 @@ def deploy_staging():
 
 @roles('production')
 def deploy_production():
-    # Remove this line when you're happy that this Fabfile is correct
+    # Remove this line when you're happy that this task is correct
     raise RuntimeError("Please check the fabfile before using it")
 
-    base_dir = '/usr/local/django/{{ project_name }}wagtail/'
-    virtualenv_dir = '/usr/local/django/virtualenvs/{{ project_name }}wagtail/'
-    python = virtualenv_dir + '/bin/python'
-    pip = virtualenv_dir + '/bin/pip'
-
-    with cd(base_dir):
-        run('git pull origin master')
-        run(pip + ' install -r requirements.txt')
-        run(python + ' manage.py migrate --settings={{ project_name }}.settings.production --noinput')
-        run(python + ' manage.py collectstatic --settings={{ project_name }}.settings.production --noinput')
-        run(python + ' manage.py compress --settings={{ project_name }}.settings.production')
-        run(python + ' manage.py update_index --settings={{ project_name }}.settings.production')
+    run('git pull origin master')
+    run('pip install -r requirements.txt')
+    run('django-admin migrate --noinput')
+    run('django-admin collectstatic --noinput')
+    run('django-admin compress')
+    run('django-admin update_index')
 
     # 'restart' should be an alias to a script that restarts the web server
     run('restart')
